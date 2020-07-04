5ae47640c04d2524351e8e70937dc09484e006a1
RobertoRamos <robram91@gmail.com>
2020-07-04 16:59:32 -0400

    progress is key

---
+++ script.js
@@ -0,0 +1,6 @@
+console.log("Breathe, you got this! Finish half and enjoy a refreshing drink later ;)")
+
+// create your nouns or variables
+let url = "https://www.thecocktaildb.com/api.php"
+
+// var for 5 images
