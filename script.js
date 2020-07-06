d8010cd882a7860843e17d8f7686b0a4623aee2f
RobertoRamos <robram91@gmail.com>
2020-07-05 20:46:17 -0400

    imgs up, need functionality, etc

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 console.log("Breathe, you got this! Finish half and enjoy a refreshing drink later ;)")
 
 // create your nouns or variables
-let url = "https://www.thecocktaildb.com/api.php"
+let url = "https://www.thecocktaildb.com/api/json/v1/1/search.php?s="
 
-// var for 5 images
