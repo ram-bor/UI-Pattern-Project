9fc5a00b8256201b1022b664d6e370c99e436b11
RobertoRamos <robram91@gmail.com>
2020-07-06 08:15:01 -0400

    saving more

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 console.log("Breathe, you got this!")
 
 // create your nouns or variables
-let url = "https://www.thecocktaildb.com/api/json/v1/1/search.php?s="
+let url = "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita"
+let url2 = "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=mint_julep"
+let url3 = "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=mojito"
+let url4 = "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=moscow%20mule"
+let url5 = "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=aperol%20spritz"
+
+
 
 // var for the drink and  modal divs
 let drink = document.querySelectorAll(".drink")
@@ -17,21 +23,66 @@ let modalImg = document.querySelectorAll("#modal-image")
 
 // var for the modal info a.k.a. the text
 let modalText = document.querySelectorAll("#info")
+console.log(modalText[0].innerText)
 
 // var for closing button to close modal
 let close = document.querySelectorAll(".close-button")
-// create for loop for every image div, then add click event listener to image to prompt the modal to open once clicked
 
+// let name = document.querySelectorAll("h3")
+
+
+
+
+// JSON data
+// modalText.addEventListener("click", loadInfo)
+
+// function loadInfo(e) {
+//     e.preventDefault()
+//     fetch(url)
+//         .then(res => {
+//             console.log("Success!", res)
+//             return res.json()
+//         })
+//         .then(data => {
+//             console.log(data.url)
+//         })
+//         .catch(err => {
+//             console.log("Error!", err)        
+//         })
+// }
+
+
+// create for loop for every image div, then add click event listener to image to prompt the modal to open once clicked
 for (let i=0; i < drink.length; i++) {
 
     image[i].addEventListener('click', function(e) {
         e.preventDefault()
+        console.log(e)
         modal[i].style.display = "block"
         modalImg[i].attributes[0].value = image[i].attributes[0].value
+
+
+        fetch(url)
+            .then(res => {
+                console.log("Success!", res)
+                return res.json()
+        })
+            .then(data => {
+                console.log(data.drinks[i].strDrink)
+                let name = data.drinks[i].strDrink
+                modalText[i].innerHTML = `You've selected ${name} !`
+        })
+            .catch(err => {
+                console.log("Error!", err)        
+        })
+        
+        
+
     }) 
 
     close[i].addEventListener('click', function(e) {
         e.preventDefault()
         modal[i].style.display = "none"
     })
+
 }
